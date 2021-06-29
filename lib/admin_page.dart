bc24410a4f8c329fc683c013184baa4852c7e389
Rockvole <rockvole@gmail.com>
2021-06-29 16:10:03 -0700

    In Approvals list use FutureBuilder

--- lib/admin_page.dart
+++ lib/admin_page.dart
@@ -8,34 +8,49 @@ class AdminPage extends StatefulWidget {
   _AdminPageState createState() => _AdminPageState();
 }
 
+class TaskStruct {
+  int id;
+  String taskName;
+  TaskStruct(this.id, this.taskName);
+}
+
 class _AdminPageState extends State<AdminPage> {
-  final List<Map> myProducts =
-      List.generate(1000, (index) => {"id": index, "name": "Product $index"})
-          .toList();
+
+  Future<List<TaskStruct>> fetchTasks() async {
+    final List<TaskStruct> tasks =
+        List.generate(1000, (index) => TaskStruct(index, "Product $index"));
+    return tasks;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Approvals'),
-        backgroundColor: Colors.blue,
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListView.builder(
-            itemCount: myProducts.length,
-            itemBuilder: (context, position) {
-              return Row(children: [
-                Text(myProducts[position]['name']),
-                Spacer(),
-                IconButton(
-                    onPressed: null,
-                    icon: Icon(Icons.cancel, color: Colors.red)),
-                IconButton(
-                    onPressed: null,
-                    icon: Icon(Icons.check_circle, color: Colors.green))
-              ]);
-            }),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Approvals'),
+          backgroundColor: Colors.blue,
+        ),
+        body: Padding(
+            padding: EdgeInsets.all(8),
+            child: FutureBuilder<List<TaskStruct>>(
+                future: fetchTasks(),
+                initialData: [],
+                builder: (BuildContext context,
+                    AsyncSnapshot<List<TaskStruct>> snapshot) {
+                  return ListView.builder(
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, position) {
+                        return Row(children: [
+                          Text(snapshot.data![position].taskName),
+                          Spacer(),
+                          IconButton(
+                              onPressed: null,
+                              icon: Icon(Icons.cancel, color: Colors.red)),
+                          IconButton(
+                              onPressed: null,
+                              icon:
+                                  Icon(Icons.check_circle, color: Colors.green))
+                        ]);
+                      });
+                })));
   }
 }
