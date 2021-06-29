d3f0abd749ef4e3cd1e0c94d7f469894222788bf
Rockvole <rockvole@gmail.com>
2021-06-29 13:26:40 -0700

    Add skeleton for Approvals List

--- lib/admin_page.dart
+++ lib/admin_page.dart
@@ -9,9 +9,24 @@ class AdminPage extends StatefulWidget {
 }
 
 class _AdminPageState extends State<AdminPage> {
-
+  final List<Map> myProducts =
+      List.generate(100000, (index) => {"id": index, "name": "Product $index"})
+          .toList();
   @override
   Widget build(BuildContext context) {
-    return Text('Approvals');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Approvals'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: ListView.builder(
+            itemCount: myProducts.length,
+            itemBuilder: (context, position) {
+              return Text(myProducts[position]['name'].toString());
+            }),
+      ),
+    );
   }
 }
