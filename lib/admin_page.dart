c168e3486c0d6b744bbc3562f5536eff73770074
Rockvole <rockvole@gmail.com>
2021-07-02 16:00:48 -0700

    In AdminPage, when there are no items for approval / rejection, show message

--- lib/admin_page.dart
+++ lib/admin_page.dart
@@ -13,10 +13,11 @@ class AdminPage extends StatefulWidget {
 class _AdminPageState extends State<AdminPage> {
   late Application _application;
 
-  Future<List<TaskHcDto>> fetchTasks() async {
+  Future<List<TaskHcDto>?> fetchTasks() async {
     AbstractDatabase db = await DataBaseAccess.getConnection();
     DbTransaction transaction = await DataBaseAccess.getTransaction();
-    List<TaskHcDto> taskHcDtoList = await _application.dbAccess.fetchTaskHcList(transaction);
+    List<TaskHcDto>? taskHcDtoList = await _application.dbAccess.fetchTaskHcList(transaction);
+    if(taskHcDtoList.length==0) taskHcDtoList=null;
     await db.close();
     return taskHcDtoList;
   }
@@ -36,26 +37,30 @@ class _AdminPageState extends State<AdminPage> {
         ),
         body: Padding(
             padding: EdgeInsets.all(8),
-            child: FutureBuilder<List<TaskHcDto>>(
+            child: FutureBuilder<List<TaskHcDto>?>(
                 future: fetchTasks(),
                 initialData: [],
                 builder: (BuildContext context,
-                    AsyncSnapshot<List<TaskHcDto>> snapshot) {
-                  return ListView.builder(
-                      itemCount: snapshot.data!.length,
-                      itemBuilder: (context, position) {
-                        return Row(children: [
-                          Text(snapshot.data![position].task_description!),
-                          Spacer(),
-                          IconButton(
-                              onPressed: null,
-                              icon: Icon(Icons.cancel, color: Colors.red)),
-                          IconButton(
-                              onPressed: null,
-                              icon:
-                                  Icon(Icons.check_circle, color: Colors.green))
-                        ]);
-                      });
+                    AsyncSnapshot<List<TaskHcDto>?> snapshot) {
+                  if(snapshot.hasData) {
+                    return ListView.builder(
+                        itemCount: snapshot.data!.length,
+                        itemBuilder: (context, position) {
+                          return Row(children: [
+                            Text(snapshot.data![position].task_description!),
+                            Spacer(),
+                            IconButton(
+                                onPressed: null,
+                                icon: Icon(Icons.cancel, color: Colors.red)),
+                            IconButton(
+                                onPressed: null,
+                                icon:
+                                Icon(Icons.check_circle, color: Colors.green))
+                          ]);
+                        });
+                  } else {
+                    return Text("No items for Approval / Rejections found", style: TextStyle(fontSize: 18));
+                  }
                 })));
   }
 }
